9f085edcfa1b1b190cc2383ef3f5ca099a7277fe
vladnovoren <vladnovoren@gmail.com>
2022-01-27 16:30:01 +0300

    init

---
+++ src/components/i_component.cpp
@@ -0,0 +1,6 @@
+#include "i_component.hpp"
+
+
+ECS::ComponentId ECS::IComponent::GetId() const {
+  return id_;
+}
---
+++ src/components/i_component.hpp
@@ -0,0 +1,26 @@
+#ifndef I_COMPONENT_HPP
+#define I_COMPONENT_HPP
+
+#include <stdint.h>
+#include <climits>
+
+
+namespace ECS {
+  enum class ComponentId {
+    UNDEFINED = -1,
+    POSITION,
+    VELOCITY
+  };
+
+  class IComponent {
+   protected:
+    ComponentId id_ = ComponentId::UNDEFINED;
+   public:
+    IComponent() = default;
+
+    ComponentId GetId() const;
+  };
+}
+
+
+#endif /* i_component.hpp*/
