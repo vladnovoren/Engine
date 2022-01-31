4025d8511ed84aa77208df066ba726cd993f6041
Андрей Кротов <andrey_krotov@vk.com>
2022-01-31 23:55:57 +0300

    Implementation of static pool allocator

--- tools/allocator.h
+++ tools/allocator.h
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <list>
 #include <memory>
+#include <memory_resource>
 #include <string>
 #include <tuple>
 #include <type_traits>
@@ -353,5 +354,88 @@ class memory_pool {
     return is_defined<id>();
   }
 };
+
+/*
+░██████╗████████╗░█████╗░████████╗██╗░█████╗░  ██████╗░░█████╗░░█████╗░██╗░░░░░
+██╔════╝╚══██╔══╝██╔══██╗╚══██╔══╝██║██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗██║░░░░░
+╚█████╗░░░░██║░░░███████║░░░██║░░░██║██║░░╚═╝  ██████╔╝██║░░██║██║░░██║██║░░░░░
+░╚═══██╗░░░██║░░░██╔══██║░░░██║░░░██║██║░░██╗  ██╔═══╝░██║░░██║██║░░██║██║░░░░░
+██████╔╝░░░██║░░░██║░░██║░░░██║░░░██║╚█████╔╝  ██║░░░░░╚█████╔╝╚█████╔╝███████╗
+╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░  ╚═╝░░░░░░╚════╝░░╚════╝░╚══════╝
+
+░█████╗░██╗░░░░░██╗░░░░░░█████╗░░█████╗░░█████╗░████████╗░█████╗░██████╗░
+██╔══██╗██║░░░░░██║░░░░░██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+███████║██║░░░░░██║░░░░░██║░░██║██║░░╚═╝███████║░░░██║░░░██║░░██║██████╔╝
+██╔══██║██║░░░░░██║░░░░░██║░░██║██║░░██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+██║░░██║███████╗███████╗╚█████╔╝╚█████╔╝██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+╚═╝░░╚═╝╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
+
+██╗███╗░░░███╗██████╗░██╗░░░░░███████╗███╗░░░███╗███████╗███╗░░██╗████████╗░█████╗░████████╗██╗░█████╗░███╗░░██╗
+██║████╗░████║██╔══██╗██║░░░░░██╔════╝████╗░████║██╔════╝████╗░██║╚══██╔══╝██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║
+██║██╔████╔██║██████╔╝██║░░░░░█████╗░░██╔████╔██║█████╗░░██╔██╗██║░░░██║░░░███████║░░░██║░░░██║██║░░██║██╔██╗██║
+██║██║╚██╔╝██║██╔═══╝░██║░░░░░██╔══╝░░██║╚██╔╝██║██╔══╝░░██║╚████║░░░██║░░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║
+██║██║░╚═╝░██║██║░░░░░███████╗███████╗██║░╚═╝░██║███████╗██║░╚███║░░░██║░░░██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║
+╚═╝╚═╝░░░░░╚═╝╚═╝░░░░░╚══════╝╚══════╝╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
+*/
+
+template <typename T = std::uint8_t, std::size_t id = 0>
+class static_pool_allocator {
+ public:
+  template <typename U>
+  struct rebind {
+    using other = static_pool_allocator<U, id>;
+  };
+
+  static_pool_allocator() noexcept : m_upstream_resource{pmr::get_default_resource()} {
+  }
+
+  static_pool_allocator(pmr::memory_resource *res) noexcept : m_upstream_resource{res} {
+  }
+
+  template <typename U>
+  static_pool_allocator(const static_pool_allocator<U, id> &other) noexcept
+      : m_upstream_resource{other.upstream_resource()} {
+  }
+
+  template <typename U>
+  static_pool_allocator &operator=(const static_pool_allocator<U, id> &other) noexcept {
+    m_upstream_resource = other.upstream_resource();
+    return *this;
+  }
+
+  static bool initialize_memory_pool() noexcept {
+    return memory_pool::initialize<id>();
+  }
+
+ private:
+  pmr::memory_resource *m_upstream_resource;
+};
+
+namespace instrument {
+template <std::size_t id, typename T, std::size_t size>
+void type_reg() {
+}
+}  // namespace instrument
+
+pointer allocate(size_type n, const void * = 0) {
+  instrument::type_reg<id, T, sizeof(T)>();
+  if constexpr (memory_pool::is_defined<id>()) {
+    return static_cast<T *>(memory_pool::allocator<id>(sizeof(T) * n));
+  } else if (m_upstream_resource != nullptr) {
+    return static_cast<T *>(m_upstream_resource->allocate(sizeof(T) * n, alignof(T)));
+  } else {
+    throw std::bad_alloc{};
+  }
+}
+
+void deallocate(T *ptr, size_type n) {
+  if constexpr (memory_pool::is_defined<id>()) {
+    memory_pool::deallocate<id>(ptr, sizeof(T) * n);
+  } else if (m_upstream_resource != nullptr) {
+    m_upstream_resource->deallocate(ptr, sizeof(T) * n, alignof(T));
+  } else {
+    assert(false);
+  }
+}
 
 #endif  // !POOLALLOCATOR_H
