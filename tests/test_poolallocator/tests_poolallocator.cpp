7c974c599684e06274ac68ab6701245818856dc2
Андрей Кротов <andrey_krotov@vk.com>
2022-02-01 03:11:59 +0300

    New test

--- tests/test_poolallocator/tests_poolallocator.cpp
+++ tests/test_poolallocator/tests_poolallocator.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
 
 #include <cstdint>
-#include "../../poolallocator/allocator.h"
 
+#include "../../poolallocator/allocator.h"
 
 /*
 Example:
@@ -18,8 +18,9 @@ TEST(BucketTestsSystem, Bucket) {
   uint8_t* p = NULL;
   uint8_t* d = NULL;
   p = (uint8_t*)(test_bucket.allocate(2));
-  d = (uint8_t*)(test_bucket.allocate(3));
-  EXPECT_EQ(d, nullptr);
+  EXPECT_FALSE(test_bucket.belongs(d));
+  EXPECT_TRUE(test_bucket.belongs(p));
   EXPECT_NE(p, nullptr);
-  delete p;
+  test_bucket.deallocate(p, 2);
+  EXPECT_TRUE(test_bucket.belongs(p));
 }
